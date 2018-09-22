18ec72cd2297da7d78afbd3602b94fe1424defad
Bobby Georgiou <32397902+bg-s-25@users.noreply.github.com>
2018-09-22 11:40:18 -0400

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,364 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox4 = New System.Windows.Forms.GroupBox()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.RadioButton3 = New System.Windows.Forms.RadioButton()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox4.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Button1)
+        Me.GroupBox1.Controls.Add(Me.TextBox1)
+        Me.GroupBox1.Location = New System.Drawing.Point(13, 11)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(497, 72)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Directory"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(15, 51)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(34, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "0 files"
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(420, 24)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(71, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Browse..."
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(15, 25)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(399, 21)
+        Me.TextBox1.TabIndex = 0
+        '
+        'FolderBrowserDialog1
+        '
+        Me.FolderBrowserDialog1.Description = "Select directory with files to be renamed..."
+        Me.FolderBrowserDialog1.ShowNewFolderButton = False
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.GroupBox4)
+        Me.GroupBox2.Controls.Add(Me.GroupBox3)
+        Me.GroupBox2.Location = New System.Drawing.Point(13, 90)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(497, 233)
+        Me.GroupBox2.TabIndex = 1
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Edits"
+        '
+        'GroupBox4
+        '
+        Me.GroupBox4.Controls.Add(Me.CheckBox2)
+        Me.GroupBox4.Controls.Add(Me.Button3)
+        Me.GroupBox4.Controls.Add(Me.TextBox4)
+        Me.GroupBox4.Controls.Add(Me.Label5)
+        Me.GroupBox4.Controls.Add(Me.TextBox5)
+        Me.GroupBox4.Controls.Add(Me.Label6)
+        Me.GroupBox4.Location = New System.Drawing.Point(252, 24)
+        Me.GroupBox4.Name = "GroupBox4"
+        Me.GroupBox4.Size = New System.Drawing.Size(228, 194)
+        Me.GroupBox4.TabIndex = 1
+        Me.GroupBox4.TabStop = False
+        Me.GroupBox4.Text = "Replace"
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Location = New System.Drawing.Point(133, 85)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(82, 17)
+        Me.CheckBox2.TabIndex = 10
+        Me.CheckBox2.Text = "Match case"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(65, 161)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(106, 23)
+        Me.Button3.TabIndex = 9
+        Me.Button3.Text = "Rename"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox4.Location = New System.Drawing.Point(44, 122)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(171, 21)
+        Me.TextBox4.TabIndex = 8
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(14, 98)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(57, 13)
+        Me.Label5.TabIndex = 7
+        Me.Label5.Text = "New text"
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox5.Location = New System.Drawing.Point(44, 60)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(171, 21)
+        Me.TextBox5.TabIndex = 6
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(14, 29)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(93, 13)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "Text to replace"
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.RadioButton3)
+        Me.GroupBox3.Controls.Add(Me.CheckBox1)
+        Me.GroupBox3.Controls.Add(Me.Label4)
+        Me.GroupBox3.Controls.Add(Me.RadioButton2)
+        Me.GroupBox3.Controls.Add(Me.RadioButton1)
+        Me.GroupBox3.Controls.Add(Me.Button2)
+        Me.GroupBox3.Controls.Add(Me.TextBox3)
+        Me.GroupBox3.Controls.Add(Me.Label2)
+        Me.GroupBox3.Controls.Add(Me.TextBox2)
+        Me.GroupBox3.Controls.Add(Me.Label1)
+        Me.GroupBox3.Location = New System.Drawing.Point(18, 24)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(228, 194)
+        Me.GroupBox3.TabIndex = 0
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "Add"
+        '
+        'RadioButton3
+        '
+        Me.RadioButton3.AutoSize = True
+        Me.RadioButton3.Location = New System.Drawing.Point(14, 78)
+        Me.RadioButton3.Name = "RadioButton3"
+        Me.RadioButton3.Size = New System.Drawing.Size(97, 17)
+        Me.RadioButton3.TabIndex = 9
+        Me.RadioButton3.TabStop = True
+        Me.RadioButton3.Text = "Append at start"
+        Me.RadioButton3.UseVisualStyleBackColor = True
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(130, 85)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(82, 17)
+        Me.CheckBox1.TabIndex = 8
+        Me.CheckBox1.Text = "Match case"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(131, 29)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(58, 13)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "this portion"
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Checked = True
+        Me.RadioButton2.Location = New System.Drawing.Point(70, 36)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(46, 17)
+        Me.RadioButton2.TabIndex = 6
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "after"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Location = New System.Drawing.Point(70, 18)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(55, 17)
+        Me.RadioButton1.TabIndex = 5
+        Me.RadioButton1.Text = "before"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(62, 161)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(106, 23)
+        Me.Button2.TabIndex = 4
+        Me.Button2.Text = "Rename"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox3.Location = New System.Drawing.Point(41, 122)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(171, 21)
+        Me.TextBox3.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(11, 98)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(82, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Text to insert"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox2.Location = New System.Drawing.Point(41, 57)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(171, 21)
+        Me.TextBox2.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(11, 29)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(54, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Add text"
+        '
+        'Timer1
+        '
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(12, 330)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(210, 13)
+        Me.Label7.TabIndex = 2
+        Me.Label7.Text = "Version 0.2.2 (Latest revision: Apr. 6, 2017)"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(522, 352)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Multiple File Renamer by BGG (v0.2.2)"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox4.ResumeLayout(False)
+        Me.GroupBox4.PerformLayout()
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents FolderBrowserDialog1 As System.Windows.Forms.FolderBrowserDialog
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents GroupBox4 As System.Windows.Forms.GroupBox
+    Friend WithEvents GroupBox3 As System.Windows.Forms.GroupBox
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents RadioButton2 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton1 As System.Windows.Forms.RadioButton
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents TextBox4 As System.Windows.Forms.TextBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents TextBox5 As System.Windows.Forms.TextBox
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents CheckBox1 As System.Windows.Forms.CheckBox
+    Friend WithEvents CheckBox2 As System.Windows.Forms.CheckBox
+    Friend WithEvents RadioButton3 As System.Windows.Forms.RadioButton
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,214 @@
+﻿'
+' Multiple File Renamer -> Class FORM1
+' Author: Bobby Georgiou
+' Date: 2017
+'
+Imports System.IO
+
+Public Class Form1
+    Public DirFileCount As Integer
+    Public DirFiles As System.Collections.ObjectModel.ReadOnlyCollection(Of String)
+    Public AddMode As String
+    Public MatchCase As Boolean
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Label3.Visible = False
+        Button2.Enabled = False
+        Button3.Enabled = False
+        GroupBox2.Enabled = False
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        FolderBrowserDialog1.ShowDialog()
+        If FolderBrowserDialog1.SelectedPath <> "" Then
+            TextBox1.Text = FolderBrowserDialog1.SelectedPath
+        Else
+            Exit Sub
+        End If
+        Label3.Visible = True
+        GroupBox2.Enabled = True
+        GetFiles()
+    End Sub
+
+    Sub GetFiles()
+        DirFiles = My.Computer.FileSystem.GetFiles(TextBox1.Text, FileIO.SearchOption.SearchTopLevelOnly)
+        DirFileCount = DirFiles.Count
+        If DirFileCount = 1 Then
+            Label3.Text = CStr(DirFileCount) & " file"
+        Else
+            Label3.Text = CStr(DirFileCount) & " files"
+        End If
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        '
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If DirFileCount = 0 Then
+            MsgBox("Cannot continue as there are zero files in the selected directory.", MsgBoxStyle.Exclamation, "Multiple File Renamer")
+        Else
+            If RadioButton1.Checked = True Then
+                AddMode = "Before"
+            ElseIf RadioButton2.Checked = True Then
+                AddMode = "After"
+            Else
+                AddMode = "Start"
+            End If
+            If CheckBox1.Checked = True Then
+                MatchCase = True
+            Else
+                MatchCase = False
+            End If
+            Dim Matchcnt As Integer = 0
+
+            If AddMode = "Start" Then
+                For i = 1 To DirFileCount
+                    Dim Ext As String = Path.GetExtension(DirFiles.Item(i - 1))
+                    Dim NewFilename As String
+                    NewFilename = Path.GetFileNameWithoutExtension(DirFiles.Item(i - 1)).Insert(0, TextBox3.Text) & Ext
+                    My.Computer.FileSystem.RenameFile(DirFiles.Item(i - 1), NewFilename)
+                    Matchcnt = Matchcnt + 1
+                Next
+            End If
+
+            If AddMode <> "Start" Then
+                For i = 1 To DirFileCount
+                    If MatchCase = True Then
+                        If Path.GetFileNameWithoutExtension(DirFiles.Item(i - 1)).Contains(TextBox2.Text) Then
+                            Dim Index As Integer = Path.GetFileNameWithoutExtension(DirFiles.Item(i - 1)).IndexOf(TextBox2.Text)
+                            Dim Ext As String = Path.GetExtension(DirFiles.Item(i - 1))
+                            Dim NewFilename As String
+                            If AddMode = "Before" Then
+                                NewFilename = Path.GetFileNameWithoutExtension(DirFiles.Item(i - 1)).Insert(Index, TextBox3.Text) & Ext
+                            Else
+                                NewFilename = Path.GetFileNameWithoutExtension(DirFiles.Item(i - 1)).Insert(CInt(Index + TextBox2.Text.Length), TextBox3.Text) & Ext
+                            End If
+                            My.Computer.FileSystem.RenameFile(DirFiles.Item(i - 1), NewFilename)
+                            Matchcnt = Matchcnt + 1
+                        End If
+                    Else
+                        If Path.GetFileNameWithoutExtension(DirFiles.Item(i - 1)).ToLower.Contains(TextBox2.Text.ToLower) Then
+                            Dim Index As Integer = Path.GetFileNameWithoutExtension(DirFiles.Item(i - 1)).IndexOf(TextBox2.Text)
+                            Dim Ext As String = Path.GetExtension(DirFiles.Item(i - 1))
+                            Dim NewFilename As String
+                            If AddMode = "Before" Then
+                                NewFilename = Path.GetFileNameWithoutExtension(DirFiles.Item(i - 1)).Insert(Index, TextBox3.Text) & Ext
+                            Else
+                                NewFilename = Path.GetFileNameWithoutExtension(DirFiles.Item(i - 1)).Insert(CInt(Index + TextBox2.Text.Length), TextBox3.Text) & Ext
+                            End If
+                            My.Computer.FileSystem.RenameFile(DirFiles.Item(i - 1), NewFilename)
+                            Matchcnt = Matchcnt + 1
+                        End If
+                    End If
+                Next
+            End If
+
+            TextBox2.Text = ""
+            TextBox3.Text = ""
+            GetFiles()
+            If Matchcnt = 0 Then
+                MsgBox("No matches found in the specified directory.", MsgBoxStyle.Information, "Multiple File Renamer")
+            ElseIf Matchcnt = 1 Then
+                MsgBox("Done with " & Matchcnt & " instance replaced.", MsgBoxStyle.Information, "Multiple File Renamer")
+            Else
+                MsgBox("Done with " & Matchcnt & " instances replaced.", MsgBoxStyle.Information, "Multiple File Renamer")
+            End If
+            End If
+    End Sub
+
+    Sub CheckTextBoxes()
+        If TextBox2.Text <> "" And TextBox3.Text <> "" Then
+            Button2.Enabled = True
+        Else
+            Button2.Enabled = False
+        End If
+
+        If TextBox3.Text <> "" And TextBox2.Text = "" And RadioButton3.Checked = True Then
+            Button2.Enabled = True
+        End If
+
+        If TextBox5.Text <> "" Then
+            Button3.Enabled = True
+        Else
+            Button3.Enabled = False
+        End If
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+        CheckTextBoxes()
+    End Sub
+
+    Private Sub TextBox3_TextChanged(sender As Object, e As EventArgs) Handles TextBox3.TextChanged
+        CheckTextBoxes()
+    End Sub
+
+    Private Sub TextBox4_TextChanged(sender As Object, e As EventArgs) Handles TextBox4.TextChanged
+        CheckTextBoxes()
+    End Sub
+
+    Private Sub TextBox5_TextChanged(sender As Object, e As EventArgs) Handles TextBox5.TextChanged
+        CheckTextBoxes()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If DirFileCount = 0 Then
+            MsgBox("Cannot continue as there are zero files in the selected directory.", MsgBoxStyle.Exclamation, "Multiple File Renamer")
+        Else
+            If CheckBox2.Checked = True Then
+                MatchCase = True
+            Else
+                MatchCase = False
+            End If
+            Dim Matchcnt As Integer = 0
+            For i = 1 To DirFileCount
+                If MatchCase = True Then
+                    If Path.GetFileNameWithoutExtension(DirFiles.Item(i - 1)).Contains(TextBox5.Text) Then
+                        Dim Ext As String = Path.GetExtension(DirFiles.Item(i - 1))
+                        Dim NewFilename As String = Path.GetFileNameWithoutExtension(DirFiles.Item(i - 1)).Replace(TextBox5.Text, TextBox4.Text) & Ext
+                        Try
+                            My.Computer.FileSystem.RenameFile(DirFiles.Item(i - 1), NewFilename)
+                        Catch ex As System.IO.IOException
+                            MsgBox(ex.Message, MsgBoxStyle.Exclamation, "Error")
+                            Exit Sub
+                        End Try
+                    End If
+                    Matchcnt = Matchcnt + 1
+                Else
+                    If Path.GetFileNameWithoutExtension(DirFiles.Item(i - 1)).ToLower.Contains(TextBox5.Text.ToLower) Then
+                        Dim Ext As String = Path.GetExtension(DirFiles.Item(i - 1))
+                        Dim NewFilename As String = Path.GetFileNameWithoutExtension(DirFiles.Item(i - 1)).Replace(TextBox5.Text, TextBox4.Text) & Ext
+                        Try
+                            My.Computer.FileSystem.RenameFile(DirFiles.Item(i - 1), NewFilename)
+                        Catch ex As System.IO.IOException
+                            MsgBox("Cannot complete the action since the specified filename already exists in the directory.", MsgBoxStyle.Exclamation, "Error")
+                            Exit Sub
+                        End Try
+                    End If
+                    Matchcnt = Matchcnt + 1
+                End If
+            Next
+            TextBox5.Text = ""
+            TextBox4.Text = ""
+            GetFiles()
+            If Matchcnt = 0 Then
+                MsgBox("No matches found in the specified directory.", MsgBoxStyle.Information, "Multiple File Renamer")
+            ElseIf Matchcnt = 1 Then
+                MsgBox("Done with " & Matchcnt & " instance replaced.", MsgBoxStyle.Information, "Multiple File Renamer")
+            Else
+                MsgBox("Done with " & Matchcnt & " instances replaced.", MsgBoxStyle.Information, "Multiple File Renamer")
+            End If
+        End If
+    End Sub
+
+    Private Sub RadioButton3_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton3.CheckedChanged
+        If RadioButton3.Checked = True Then
+            TextBox2.Enabled = False
+            CheckBox1.Enabled = False
+        Else
+            TextBox2.Enabled = True
+            CheckBox1.Enabled = True
+        End If
+        CheckTextBoxes()
+    End Sub
+End Class
